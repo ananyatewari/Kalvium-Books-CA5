c6e3f769baa7589dad75918019534e1d17bd0a64
ananyatewari <ananyatewari0205@gmail.com>
2024-02-07 12:08:55 +0530

    done

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -10,7 +10,7 @@ export default function Library({ input, showInput }) {
   useEffect(() => {
     axios
       .get("https://reactnd-books-api.udacity.com/books", {
-        headers: { Authorization: "whatever-you-want" },
+        headers: { "Authorization": "whatever-you-want" },
       })
       .then((response) => {
         console.log(response.data.books);
